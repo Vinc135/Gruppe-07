654d48a1f929f7c07010c664c847461ae8548fee
LeHol <l3nnox.hol@gmial.com>
2026-05-18 14:54:33 +0200

    created auto_hinzufuegen

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import curses
 from action_type_attribute import *
 from garage import Garage 
+from auto import Auto
 
 def draw_menu(stdscr):
 
@@ -197,3 +198,53 @@ def auto_detail_menu(stdscr, kennzeichen):
 def delete_and_refresh(stdscr, garage, kennzeichen):
     garage.auto_entfernen(kennzeichen)
     autos_anzeigen(stdscr)
+
+
+def auto_hinzufuegen(stdscr):
+    curses.curs_set(1) # setzt curser auf sichtbar
+    stdscr.clear()
+
+    fields = [
+        ("kennzeichen", "Kennzeichen"),
+        ("marke", "Marke"),
+        ("modell", "Modell"),
+        ("baujahr", "Baujahr"),
+        ("kilometer", "Kilometer"),
+        ("verbrauch", "Verbrauch"),
+        ("tagespreis", "Tagespreis"),
+    ]
+
+    inputs = {}
+    for i, (key, label) in enumerate(fields): # imput für jede mögliche eingabe eines elements aus fileds
+        stdscr.addstr(i, 0, f"{label}: ") # neue zeile für input unter dem letzten
+        stdscr.refresh()
+        curses.echo() # macht eingabe sichtbar
+        val = stdscr.getstr(i, len(label) + 2).decode("utf-8") # liest eingabe ein an stelle i der konsole nach dem label +2 für ": "
+        curses.noecho() # macht eingabe unsichtbar
+        inputs[key] = val # setzt value
+
+    baujahr = int(inputs.get("baujahr") or 0)
+    kilometer = int(inputs.get("kilometer") or 0)
+    verbrauch = float(inputs.get("verbrauch") or 0)
+    tagespreis = float(inputs.get("tagespreis") or 0)
+
+    auto = Auto(
+        inputs.get("kennzeichen") or "Kein Kennzeichen angegeben",
+        inputs.get("marke") or "Keine Marke gesetzt",
+        inputs.get("modell") or "Kein Modell angegeben",
+        baujahr,
+        kilometer,
+        verbrauch,
+        tagespreis,
+        verliehen=False,
+        verliehen_bis=0,
+    )
+
+    garage = Garage()
+    garage.auto_hinzufügen(auto)
+
+    stdscr.addstr(len(fields) + 1, 0, "Auto erfolgreich hinzugefügt. Drücke eine Taste, um zurückzugehen.")
+    stdscr.refresh()
+    stdscr.getch() # wartet auf userinput
+
+    return main_menu()
